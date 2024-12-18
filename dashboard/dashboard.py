a916504d3522d720d97b7eea632399dbaf0b5e0c
Rahmatuloh Aldika <rahmatulohaldika421@gmail.com>
2024-12-18 13:11:34 +0700

    submit new-1

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,40 +1,40 @@
-import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import streamlit as st
+sns.set(style='dark')
 
-# Load the data
-all_df = pd.read_csv("./all_data.csv")
+dongsi_df = pd.read_csv('dongsi_pm25.csv')
+all_df = pd.read_csv('all_pm25.csv')
 
-# Set the title of the dashboard
-st.title('E-commerce Data Analysis Dashboard')
+st.header('Kualitas Udara Sesuai Kadar PM2.5')
 
-# Sidebar for navigation
-menu = ['Home', 'Top Products', 'Sales by State', 'Review Scores']
-choice = st.sidebar.selectbox('Menu', menu)
+st.subheader('Perbandingan Kadar PM2.5 di Station Dongsi pada Tahun 2015 dan 2016')
+fig, ax = plt.subplots(figsize=(15, 6))
 
-if choice == 'Home':
-    st.subheader('Home')
-    st.write('Welcome to the E-commerce Data Analysis Dashboard. Use the sidebar to navigate through different sections.')
+colors = ['#D3D3D3', '#72BCD4']
 
-elif choice == 'Top Products':
-    st.subheader('Top Products')
-    # Plot top 5 highest profit products
-    fig, ax = plt.subplots(figsize=(10, 6))
-    sns.barplot(x="price", y="product_category_name_english", data=all_df, estimator=sum, ci=None, palette="Blues_d", order=all_df.groupby("product_category_name_english")["price"].sum().sort_values(ascending=False).index[:5])
-    ax.set_xlabel('Price')
-    ax.set_ylabel('Product Category')
-    ax.set_title('Top 5 Highest Profit Products')
-    st.pyplot(fig)
+sns.barplot(x='year', y='PM2.5', data=dongsi_df, palette=colors)
+ax.set_ylabel('PM2.5')
+ax.set_xlabel('Year', fontsize=15)
+ax.set_title('Average PM2.5 in Dongsi', loc='center', fontsize=20)
+ax.tick_params(axis='y', labelsize=12)
 
-elif choice == 'Sales by State':
-    st.subheader('Sales by State')
-    # Plot number of orders by state
-    state_counts = all_df['customer_state'].value_counts()
-    st.bar_chart(state_counts)
 
-elif choice == 'Review Scores':
-    st.subheader('Review Scores')
-    # Plot distribution of review scores
-    score_counts = all_df['review_score'].value_counts().sort_index()
-    st.bar_chart(score_counts)
+plt.tight_layout()
+st.pyplot(fig)
+
+
+st.subheader('Daftar Station Berdasarkan Kadar PM2.5 Pada Tahun 2013 - 2017')
+fig, ax = plt.subplots(figsize=(15, 6))
+
+colors = ['#D3D3D3', '#72BCD4', '#D3D3D3', '#D3D3D3', '#D3D3D3', '#D3D3D3', '#D3D3D3', '#D3D3D3', '#D3D3D3', '#D3D3D3', '#D3D3D3']
+
+sns.barplot(x='PM2.5', y='station', data=all_df, palette=colors)
+ax.set_ylabel('Station')
+ax.set_xlabel('PM2.5', fontsize=15)
+ax.set_title('Average PM2.5 in All Station', loc='center', fontsize=20)
+ax.tick_params(axis='y', labelsize=12)
+
+plt.tight_layout()
+st.pyplot(fig)
